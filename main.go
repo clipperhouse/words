8b848295ee733c19cf6ac7f58492fd736f8cd7f0
Matt Sherman <mwsherman@gmail.com>
2022-05-26 13:56:26 -0400

    Make it presentable

--- main.go
+++ main.go
@@ -9,45 +9,91 @@ import (
 	"github.com/clipperhouse/uax29/words"
 )
 
+type config struct {
+	In        *bufio.Reader
+	HasIn     bool
+	Delimiter []byte
+	Out       *bufio.Writer
+}
+
 func main() {
-	fi, err := os.Stdin.Stat()
-	handle(err)
-	piped := (fi.Mode() & os.ModeCharDevice) == 0 // https://stackoverflow.com/a/43947435/70613
-
-	out := bufio.NewWriter(os.Stdout)
-
-	if !piped {
-		out.WriteString("greetings\n")
-		out.Flush()
-		os.Exit(0)
+	config, err := getConfig()
+	if err != nil {
+		handle(err)
 	}
 
-	delimiter := []byte("\n")
+	if !config.HasIn {
+		message := "words handles piped input, splits into one word per line, and outputs to std out. use cat or echo to get started."
+		config.Out.WriteString(message)
+		goto finish
+	}
 
-	in := bufio.NewReader(os.Stdin)
+	err = writeWords(config)
+	if err != nil {
+		handle(err)
+	}
 
+finish:
+	// Finish up
+	final := []byte("\n")
+	_, err = config.Out.Write(final)
+	if err != nil {
+		handle(err)
+	}
+
+	err = config.Out.Flush()
+	if err != nil {
+		handle(err)
+	}
+}
+
+func getConfig() (*config, error) {
+	c := &config{
+		Out:       bufio.NewWriter(os.Stdout),
+		Delimiter: []byte("\n"),
+	}
+
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	piped := (fi.Mode() & os.ModeCharDevice) == 0 // https://stackoverflow.com/a/43947435/70613
+	if piped {
+		c.In = bufio.NewReader(os.Stdin)
+		c.HasIn = true
+	}
+
+	return c, nil
+}
+
+func writeWords(c *config) error {
 	first := true
-	sc := words.NewScanner(in)
+	sc := words.NewScanner(c.In)
 	sc.Filter(filter.Wordlike)
 
 	for sc.Scan() {
 		if !first {
-			_, err := out.Write(delimiter)
-			handle(err)
+			_, err := c.Out.Write(c.Delimiter)
+			if err != nil {
+				return err
+			}
 		}
 		first = false
-		_, err := out.Write(sc.Bytes())
-		handle(err)
-	}
-	handle(sc.Err())
 
-	_, err = out.WriteRune('\n')
-	handle(err)
-	handle(out.Flush())
+		_, err := c.Out.Write(sc.Bytes())
+		if err != nil {
+			return err
+		}
+	}
+
+	if sc.Err() != nil {
+		return sc.Err()
+	}
+
+	return nil
 }
 
 func handle(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
+	log.Fatalln(err)
 }
